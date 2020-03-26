a21e2121faaa041ac76ae29d08d61b4519638878
herea.farid <herea.farid@gmail.com>
2020-03-26 16:49:07 +0200

    first commit

---
+++ Exercise Files/03_02/Begin/process.js
@@ -0,0 +1,19 @@
+// main thread
+const worker = new Worker('worker.js', {name: "cities"});
+worker.addEventListener('message', event => {
+    switch(event.data.action) {
+        case 'output':
+            console.log('output received');
+            document.querySelector("#output").innerHTML = event.data.data;
+            break;
+        case 'progress':
+            document.querySelector("progress").value = event.data.data;
+            break;
+    }
+})
+
+function start() {
+    worker.postMessage({
+        action: "start"
+    })
+}
